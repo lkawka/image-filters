acab1a753a0f226ad9c5ee98c5bd035eaa76fc0f
lukkaw <lukasz.kawka@syncron.com>
2020-03-29 17:37:40 +0200

    Dithering fixed

--- ImageFilters/filters/AverageDitheringFilter.cs
+++ ImageFilters/filters/AverageDitheringFilter.cs
@@ -18,19 +18,43 @@ namespace ImageFilters.filters
 
         public override FastImage Apply()
         {
-            Image.SetAll(px => (GetChannelValue(px.r, ColorsCount.r), GetChannelValue(px.g, ColorsCount.g), GetChannelValue(px.b, ColorsCount.b)));
+
+            List<(int r, int g, int b)> pxs = Image.GetAll().ToList();
+
+            var rPoints = GetDividingPoints(pxs.Select(px => px.r).ToList(), ColorsCount.r);
+            var gPoints = GetDividingPoints(pxs.Select(px => px.g).ToList(), ColorsCount.g);
+            var bPoints = GetDividingPoints(pxs.Select(px => px.b).ToList(), ColorsCount.b);
+
+            Image.SetAll(px => (GetChannelValue(px.r, rPoints), GetChannelValue(px.g, gPoints), GetChannelValue(px.b, bPoints)));
 
             return Image;
         }
 
-        private int GetChannelValue(int org, int k)
+        private int GetChannelValue(int org, List<int> points)
         {
-            double t = 255 / (k - 1);
-            for (double i = t / 2, val = 0; i < 255; i += t, val += t)
+            double val = 0, t = 255 / points.Count;
+            for (int i = 0; i < points.Count; i += 1)
             {
-                if (org <= i) return (int)val;
+                if (org <= points[i])
+                    return (int)val;
+                val += t;
             }
             return 255;
+        }
+
+        private List<int> GetDividingPoints(List<int> values, int k)
+        {
+            var points = new List<int>();
+            if (k == 1 || values.Count < 2)
+                return points;
+
+            int average = (int)values.Average();
+
+            points.AddRange(GetDividingPoints(values.Where(v => v <= average).ToList(), k - 1));
+            points.Add(average);
+            points.AddRange(GetDividingPoints(values.Where(v => v > average).ToList(), k - 1));
+
+            return points;
         }
     }
 }
